47bc7eed5a68ec8711b00e92f34e7ba829f44031
mnsapits <mnsapits@umich.edu>
2016-11-15 20:56:35 -0800

    Add cards to DOM

---
+++ src/Game.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Card from './Card';
+import './Game.css';
+
+
+import anemoneImg from './images/anemone.jpg';
+import clownfishImg from './images/clownfish.jpg';
+import mushroomImg from './images/mushroom.jpg';
+import seahorseImg from './images/seahorse.jpg';
+import shrimpImg from './images/shrimp.jpg';
+import starfishImg from './images/starfish.jpg';
+import tangImg from './images/tang.jpg';
+import urchinImg from './images/urchin.jpg';
+
+class Game extends Component {
+  constructor() {
+    super();
+    this.deck = [
+      ['Anemone', anemoneImg],
+      ['Clownfish', clownfishImg],
+      ['Mushroom Coral', mushroomImg],
+      ['Seahorse', seahorseImg],
+      ['Cleaner Shrimp', shrimpImg],
+      ['Starfish', starfishImg],
+      ['Blue Hippo Tang', tangImg],
+      ['Sea Urchin', urchinImg]
+  ];
+  }
+
+  cards() {
+    const result = [];
+    this.deck.forEach((pair, i) => {
+      pair.forEach((cardVal, j) => {
+        if(j === 0){
+          result.push(<Card
+            matchId={i}
+            type='text'
+            value={cardVal}
+            key={cardVal}
+            />
+          );
+        } else {
+          result.push(<Card
+            matchId={i}
+            type='url'
+            value={cardVal}
+            key={cardVal}
+            />
+          );
+        }
+      });
+    });
+    return (result);
+  }
+
+  render() {
+    return (
+      <div className="game-board">
+        {this.cards()}
+      </div>
+    );
+  }
+}
+
+export default Game;
